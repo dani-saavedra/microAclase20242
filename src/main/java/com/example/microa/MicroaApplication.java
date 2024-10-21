bd8fe6c14906e9386e1fce0002b1f72b1de2de5c
Daniel Saavedra <daniel.saavedra.fon@gmail.com>
2024-10-21 08:38:04 -0500

    Initial commit

---
+++ src/main/java/com/example/microa/MicroaApplication.java
@@ -0,0 +1,13 @@
+package com.example.microa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/microa/MicroaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
